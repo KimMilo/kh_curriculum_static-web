f13c667418a8d29367183d21d49ced1fa5332c38
KimMilo <tl123612@gmail.com>
2023-04-30 13:29:08 +0900

    kh_교육과정_Static-Web 실습

--- static_web02/webapp/static/js/temp.js
+++ static_web02/webapp/static/js/temp.js
@@ -1,36 +1,63 @@
-$(document).ready(function() {
-    $(".list-group").eq(0).find(".list-group-item").on("click", sideMenuActive);
-    $(".pagination").find(".page-item").on("click", pagingActive);
-}); 
+$(document).ready(function () {
+    $("ul").eq(2).on("click", function(e) { active1(e); });
+    $("ul").eq(3).on("click", function(e) { active2(e); });
+    $("div > a").on("click", function(e) { active3(e); });
+    $("ul").eq(0).children().children().on("click", function(e) { active4(e); });
+    $("tbody > tr").hover(
+        function(e) { 
+          $(e.target).parent().css("background-color", "orange");
+          $(e.target).parent().css("text-decoration", "underline");
+        },
+        function(e) { 
+            $(e.target).parent().removeAttr("style", "background-color"); 
+        }
+      ); 
+});
 
-function sideMenuActive(event) {
-    $(event.target).siblings(".active").removeClass("active"); /*.siblings 모든 형제들 */
-    $(event.target).addClass("active");
-    $("section h4").text($(event.target).text());
+function active1(e) {
+  $("a").parent().parent("ul").eq(2).children().removeClass("active");
+   $("a").parent().parent("ul").eq(3).children().removeClass("active");
+   $(e.target).parent("li").addClass("active");
+   $(e.target).parent("li").prevAll().removeClass("active");
+   $(e.target).parent("li").nextAll().removeClass("active");
+    document.getElementsByName("title")[0].innerText = e.target.innerText;   
 }
+function active2(e) {
+  $("a").parent().parent("ul").eq(2).children().removeClass("active");
+   $("a").parent().parent("ul").eq(3).children().removeClass("active");
+   $(e.target).parent("li").addClass("active");
+   $(e.target).parent("li").prevAll().removeClass("active");
+   $(e.target).parent("li").nextAll().removeClass("active");
+    document.getElementsByName("title")[0].innerText = e.target.innerText;   
+}
+ function active3(e) {
+    $(e.target).css("color", "blue");
+    $(e.target).nextAll().css("color", "white");
+    $(e.target).prevAll().css("color", "white");  
+ }
+ function active4(e) {
+    $(e.target).removeClass("text-light");
+    $(e.target).css("text-decoration", "underline");
+    $(e.target).parent().nextAll().children().addClass("text-light");
+    $(e.target).parent().prevAll().children().addClass("text-light"); 
+    $(e.target).parent().nextAll().children().removeAttr("style", "text-decoration");
+    $(e.target).parent().prevAll().children().removeAttr("style", "text-decoration");
+ }
+function changeValue(e) {
+  var langSelect = document.getElementById("id-lang");
 
-function pagingActive(event) {
-    /* currentTarget 과 target의 차이 
-       target 를 해당 엘리먼트
-       currentTarget은 이벤트가 동작(이벤트를 등록한)하는 엘리먼트.
-       
-       아래 예시로 설명하면, 이벤트 등록을 .page-item 에 했기 때문에
-       target은 a태그 엘리먼트가 잡힐거고, currentTarget은 li태그 엘리먼트가 잡히게됨.
-       */
-    if(event.currentTarget == $(event.currentTarget).parent().children()[0]) {
-        let active = $(event.currentTarget).siblings(".active");
-        if(event.currentTarget != active.prev()[0]) {
-            active.prev().addClass("active");
-            active.removeClass("active");
-        }
-    } else if(event.currentTarget == $(event.currentTarget).parent().children().last()[0]) {
-        let active = $(event.currentTarget).siblings(".active");
-        if(event.currentTarget != active.next()[0]) {
-            active.next().addClass("active");
-            active.removeClass("active");
-        }
-    } else {
-        $(event.currentTarget).siblings(".active").removeClass("active");
-        $(event.currentTarget).addClass("active");
-    }
+  /* 
+    .options[langSelect.selectedIndex].value; : 선택된 옵션의 인덱스번호의 value 반환
+    .options[langSelect.selectedIndex].text; : 선택된 옵션의 인덱스번호의 text 반환
+  */
+  //var selectValue = langSelect.options[langSelect.selectedIndex].value;
+  var selectText = langSelect.options[langSelect.selectedIndex].text;
+  
+  if(selectText === '5개') {
+    $("tbody > tr").eq(4).nextAll().css("display", "none");
+    $("table").css("height", "305px");
+  } else {
+    $("tbody > tr").removeAttr("style");
+    $("table").removeAttr("sytle","height");
+  }
 }
--- static_web02/webapp/static/js/temp2.js
+++ static_web02/webapp/static/js/temp2.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 });
 
 function saveButtonEnable(event, target) {
-    /* event는 saveButtonEnable이고 $(e.target)은 해당 이벤트의 타겟인 select, input 임 */
+    let e = $(event.target);
     let t = $(target);
     t.attr("disabled", false);
     t.removeClass("btn-outline-secondary");
@@ -18,11 +18,8 @@ function saveButtonEnable(event, target) {
 function createAddressForm(element) {
     let e = $(element);
     let tagText = '<div id="addressForm" class="card border-0 mb-3"><div class="mb-2 text-end dropdown custom-dropdown"><a class="dropdown-toggle fw-bold" role="button" data-bs-toggle="dropdown"><span class="material-symbols-outlined">more_vert</span></a><ul class="dropdown-menu"><li><a class="dropdown-item text-danger fw-bold" role="button" onclick="removeAddressForm(this, \'#addressForm\');">삭제</a></li></ul></div><div class="row mb-2"><label class="col-2 col-form-label">주소 분류</label><div class="col-4"><select name="addressType" class="form-select"><option value="home" selected>집</option><option value="company">회사</option><option value="other">기타</option></select></div><label class="col-2 col-form-label">우편번호</label><div class="col-4"><input type="text" class="form-control" name="birthday" value=""></div></div><div class="row mb-2"><label class="col-2 col-form-label">주소</label><div class="col-10"><input type="text" class="form-control" name="address" value=""></div></div></div>'
-    /* jQuery 에서는 태그들 문자열로 넣어주면 html로 작업해서 태그들 만들어줌 
-       이 때, '' "" 겹치지 않게 사용해야 에러안나고 때에 따라 \ escape해서 활용할 것. */
-
     let addressForm = $(tagText);
-    addressForm.find("input, select").on("change", function(e) { saveButtonEnable(e, element.nextElementSibling); });
+    addressForm.find("input,select").on("change", function(e) { saveButtonEnable(e, element.nextElementSibling); });
     e.parent().before(addressForm);
 }
 
@@ -30,3 +27,9 @@ function removeAddressForm(element, id) {
     let e = $(element);
     e.parents(id).remove();
 }
+
+// function sideMenuActive(event) {
+//     $(event.target).siblings(".active").removeClass("active");
+//     $(event.target).addClass("active");
+//     event.stopimmediatepropagation();
+// }
